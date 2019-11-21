1daed3060ece7a6a651a052e2d4d81a04d3cbe2c
joeynap <joeynap@hotmail.com>
2019-11-21 18:48:06 +0100

    Aas is 11 of 1; kaarten worden juist weergegeven

--- src/blackjack.java
+++ src/blackjack.java
@@ -33,35 +33,61 @@ class blackjack {
 	}
 	
 	void spelen(card[] cards) {
+		//pak beginkaarten en laat zien
 		ArrayList<card> hand = new ArrayList<>();
 		ArrayList<card> dealer = new ArrayList<>();
 		start(cards, hand, dealer);
-		showHands(hand, dealer);
-		game:for (int beurt = 4; beurt < 52; beurt++) { //beurt < cards.length
+		//check of dealer/speler blackjack heeft
+		if (countTotal(hand) == 21) {
+			showHands(hand, dealer);
+			System.out.println("U heeft blackjack!");
+			return;
+		} else if (countTotal(dealer) == 21) {
+			showHands(hand, dealer);
+			System.out.println("Dealer heeft blackjack.");
+			return;
+		}
+		showHands(hand, dealer, true);
+		
+		//geef speler opties
+		for (int beurt = 4; beurt < 52; beurt++) { //beurt < cards.length
 			// user input
 			String input = getInput();
 			if (input.equals("k")) { // kaart trekken
 				hand.add(cards[beurt]);
-				showHands(hand, dealer);
 				if (countTotal(hand) == 21) {
-					System.out.println("U heeft blackjack!");
-					break game;
-				} else if(countTotal(hand) > 21) {
-					System.out.println("U bent bust. De dealer wint.");
-				}
-			} else if(input.equals("q")) { // stoppen
-				break;
-			} else if(input.equals("p")) {
-				while (countTotal(dealer) <= 17) {
-					dealer.add(cards[beurt]);
 					showHands(hand, dealer);
+					System.out.println("U heeft blackjack!");
+					break;
+				} else if(countTotal(hand) > 21) {
+					showHands(hand, dealer);
+					System.out.println("U bent bust. De dealer wint.");
+					break;
+				}
+				showHands(hand, dealer, true);
+				
+			} else if(input.equals("q")) { //stoppen
+				break;
+				
+			} else if(input.equals("p")) {
+				while (countTotal(dealer) <= 17 || countTotal(dealer) <= countTotal(hand)) {
+					dealer.add(cards[beurt]);
+					beurt++;
 				}
 				if (countTotal(dealer) == 21) {
+					showHands(hand, dealer);
 					System.out.println("Dealer heeft blackjack.");
-					break game;
 				} else if(countTotal(dealer) > 21) {
+					showHands(hand, dealer);
 					System.out.println("De dealer is bust. U Wint!");
+				} else if (countTotal(dealer) > countTotal(hand)) {
+					showHands(hand, dealer);
+					System.out.println("De dealer wint.");
+				} else {
+					showHands(hand, dealer);
+					System.out.println("U wint!");
 				}
+				break;
 			}
 		}
 	}
@@ -78,9 +104,23 @@ class blackjack {
 	}
 	
 	void showHands(ArrayList<card> hand, ArrayList<card> dealer) {
-		System.out.println("Dealer: " + dealer.get(0).type + dealer.get(0).value + " "
-	+ dealer.get(1).type + dealer.get(1).value);
+		System.out.print("Dealer: ");
+		for (int kaart = 0; kaart < dealer.size(); kaart++) {
+			System.out.print(dealer.get(kaart).type + dealer.get(kaart).value + " ");
+		}
+		System.out.println();
 		System.out.println("Value: " + countTotal(dealer));
+		System.out.print("Hand: ");
+		for (int kaart = 0; kaart < hand.size(); kaart++) {
+			System.out.print(hand.get(kaart).type + hand.get(kaart).value + " ");
+		}
+		System.out.println();
+		System.out.println("Value: " + countTotal(hand));
+	}
+	
+	void showHands(ArrayList<card> hand, ArrayList<card> dealer, boolean hidden) {
+		System.out.println("Dealer: " + dealer.get(0).type + dealer.get(0).value + " ?");
+		System.out.println("Value: ?");
 		System.out.print("Hand: ");
 		for (int kaart = 0; kaart < hand.size(); kaart++) {
 			System.out.print(hand.get(kaart).type + hand.get(kaart).value + " ");
@@ -100,8 +140,15 @@ class blackjack {
 	
 	int countTotal(ArrayList<card> cards) {
 		int total = 0;
+		boolean aasInHand = false;
 		for (int card = 0; card < cards.size(); card++) {
 			total += cards.get(card).getValue();
+			if (cards.get(card).value.equals("Aas")) {
+				aasInHand = true;
+			}
+		}
+		if (total > 21 && aasInHand) {
+			total-=10;
 		}
 		return total;
 	}
--- src/card.java
+++ src/card.java
@@ -7,7 +7,7 @@ public class card {
 		if (this.value.equals("Boer") || this.value.equals("Vrouw") || this.value.equals("Koning")) {
 			return 10;
 		} else if(this.value.equals("Aas")) {
-			return 1;
+			return 11;
 		}
 		return Integer.parseInt(this.value);
 	}
